7b6a4e8a371449043193353a7d4127113d1588a6
Zac <zaccannon2003@gmail.com>
2023-04-14 16:21:23 +1000

    added serverport and connection class

---
+++ DroneServer/src/main/java/com/mycompany/droneserver/DroneServer.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.droneserver;
+
+/**
+ *
+ * @author Zac
+ */
+import java.net.*;
+import java.io.*;
+
+public class DroneServer {
+
+    public static void main(String[] args) {
+
+        try {
+            int serverPort = 7896;
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+            while (true) {
+                Socket clientSocket = listenSocket.accept();
+                Connection c = new Connection(clientSocket);
+                System.out.printf("\nServer waiting on: %d for client from %d ",
+                        listenSocket.getLocalPort(), clientSocket.getPort());
+            }
+        } catch (IOException e) {
+            System.out.println("Listen :" + e.getMessage());
+        }
+    }
+}
+
+class Connection extends Thread {
+      DataInputStream in;
+      DataOutputStream out;
+      Socket clientSocket;
+      public Connection (Socket aClientSocket) {
+        try {
+          clientSocket = aClientSocket;
+          in=new DataInputStream( 
+                    clientSocket.getInputStream());
+          out=new DataOutputStream(
+                    clientSocket.getOutputStream());
+          this.start();
+        } catch(IOException e){
+           System.out.println("Connection:" +e.getMessage());
+          }
+      }
+     public void run(){
+        try { // an echo server
+           String data = in.readUTF();
+         // System.out.println(data);
+          out.writeUTF("Server received:"+data);
+
+        }catch(EOFException e) {
+             System.out.println("EOF:"+e.getMessage());
+        }
+        catch(IOException e){
+           System.out.println("IO:"+e.getMessage());
+        }
+	 
+	finally {
+	   try {clientSocket.close();
+           }
+	    catch(IOException e){/*close failed*/
+           }
+        }
+     }
+}
