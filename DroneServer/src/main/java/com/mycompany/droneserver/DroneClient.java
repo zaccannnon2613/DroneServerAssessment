a38b8e747f6094b3d5ab7b5443b860514884e919
Zac <zaccannon2003@gmail.com>
2023-04-14 16:22:04 +1000

    added server port and exception handling

--- DroneServer/src/main/java/com/mycompany/droneserver/DroneClient.java
+++ DroneServer/src/main/java/com/mycompany/droneserver/DroneClient.java
@@ -8,6 +8,35 @@ package com.mycompany.droneserver;
  *
  * @author Zac
  */
+import java.net.*;
+import java.io.*;
 public class DroneClient {
     
-}
+    public static void main (String args[]){
+        Socket s=null;
+        String hostName = "localhost";
+        String message = "Hello";
+        
+        try{
+            int serverPort = 7896;
+            
+            s = new Socket(hostName, serverPort);
+            
+        }
+        catch (UnknownHostException e){
+	   System.out.println("Sock:"+e.getMessage()); 
+	} catch (EOFException e){
+	   System.out.println("EOF:"+e.getMessage());
+    	} catch (IOException e){
+	   System.out.println("IO:"+e.getMessage());
+        }
+	finally {
+	   if(s!=null)
+	     try {
+              s.close();
+             } catch (IOException e){
+		System.out.println("close:"+e.getMessage());}
+        } 
+    }
+    }
+
